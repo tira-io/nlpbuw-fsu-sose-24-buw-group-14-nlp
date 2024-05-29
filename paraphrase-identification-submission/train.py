b329ab6046852d4fc9cadd031d554ac34ea9d5fc
Darshan Dodamani <58951142+darshandodamani@users.noreply.github.com>
2024-05-29 20:55:16 +0000

    modify path dir

--- paraphrase-identification-submission/train.py
+++ paraphrase-identification-submission/train.py
@@ -1,9 +1,12 @@
 from tira.rest_api_client import Client
 from cosine_similarity import compute_cosine_similarity
+from tira.third_party_integrations import get_output_directory
 import pandas as pd
 from sklearn.metrics import matthews_corrcoef
 import numpy as np
-import joblib
+# import joblib
+from pathlib import Path
+from joblib import dump
 
 if __name__ == "__main__":
 
@@ -32,4 +35,6 @@ if __name__ == "__main__":
     print(f"Best MCC: {best_mcc}")
 
     # Save the best threshold
-    joblib.dump(best_threshold, r"paraphrase-identification-submission/model.joblib")
+    output_directory = get_output_directory(str(Path(__file__).parent))
+    dump(best_threshold, Path(output_directory) / "model.joblib")
+    # joblib.dump(best_threshold, r"paraphrase-identification-submission/model.joblib")
