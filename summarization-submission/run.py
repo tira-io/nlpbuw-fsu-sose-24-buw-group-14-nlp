14d4062d653cafad68e1878cf81c7e3f5a35aebc
Darshan Dodamani <58951142+darshandodamani@users.noreply.github.com>
2024-06-12 19:04:05 +0000

    fixed folder structure issue found in docker build

--- summarization-submission/run.py
+++ summarization-submission/run.py
@@ -1,4 +1,10 @@
-from src.main import main
+import sys
+from pathlib import Path
+
+# Add the src directory to the Python path
+sys.path.append(str(Path(__file__).resolve().parent / "src"))
+
+from main import main
 
 if __name__ == "__main__":
     main()
