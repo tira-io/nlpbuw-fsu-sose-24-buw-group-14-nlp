5c9e8d2b14186feec466c7db9a1951e17dc4f84c
Darshan Dodamani <58951142+darshandodamani@users.noreply.github.com>
2024-06-25 20:27:54 +0000

    necessary path is included

--- ner-submission/run.py
+++ ner-submission/run.py
@@ -4,9 +4,9 @@ from pathlib import Path
 from tira.rest_api_client import Client
 from tira.third_party_integrations import get_output_directory
 
-def load_model(model_path):
-    """ Load the trained spaCy NER model from a directory. """
-    return spacy.load(model_path)
+def load_model(model_name):
+    """ Load the trained spaCy NER model by name. """
+    return spacy.load(model_name)
 
 def process_texts(nlp, texts):
     """ Process a list of texts and return the NER tags using BIO format. """
@@ -29,9 +29,9 @@ if __name__ == "__main__":
     text_data = tira.pd.inputs("nlpbuw-fsu-sose-24", "ner-validation-20240612-training")
     sentences = text_data['sentence'].tolist()
 
-    # Load the spaCy model
-    model_path = "/workspaces/nlpbuw-fsu-sose-24-buw-group-14-nlp/ner-submission/ner_model/en_core_web_sm/en_core_web_sm-3.7.1"
-    nlp = spacy.load(model_path)
+    # Load the spaCy model by name
+    model_name = "en_core_web_sm"
+    nlp = load_model(model_name)
 
     # Process the data
     predictions_tags = process_texts(nlp, sentences)
